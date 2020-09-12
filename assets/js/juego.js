536919e2ee219d06af75e6cc2886af8d1ff4a6c2
vannesa <manelp3214@gmail.com>
2020-09-11 20:42:38 -0500

    valores aleatorios

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -15,9 +15,32 @@ const creaeDeck = () => {
         }
     
 
-    console.log( deck );
+    //console.log( deck );
     deck = -.shuffle( deck );
     console.log( deck );
     return deck;
 } 
 crearDeck();
+
+const pedirCarta = () => {
+
+    if ( deck.length === 0) {
+        throw 'No hay cartas en el deck';
+    }
+
+const carta = deck.pop();
+
+    console.log(deck);
+    console.log(carta); 
+    return carta;
+}
+    //pedirCarta();
+const valorCarta = ( carta ) => {
+    const valor = carta.substring(0, carta.length-1);
+    return ( isNaN( valor )) ?
+   ( valor === 'A' ) ? 11 : 10;
+   : valor * 1;
+  
+}
+const valor = valorCarta( pedirCarta() );
+console.log({valor});
